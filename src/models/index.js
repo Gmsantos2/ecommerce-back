fb2771819ea312f8de5ab187666dfd199c353e02
Gmsantos2 <gmsantos2@utpl.edu.ec>
2024-02-21 22:49:41 -0500

    add products

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,6 @@
 const Category = require('./Category')
 const User = require('./User');
+const Product = require('./Product')
+
+Product.belongsTo(Category) // Product -> categoryId
+Category.hasMany(Product)
