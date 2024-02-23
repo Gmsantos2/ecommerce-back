addf09728fb2909f6474d338c3bb6d1b5582997b
Gmsantos2 <gmsantos2@utpl.edu.ec>
2024-02-22 22:21:24 -0500

    add test product

--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -64,14 +64,32 @@ test('GET -> URL_BASE, should return res.status code 200, res.body to be defined
     expect(res.body[0].category.id).toBe(category.id)
 })
 
+test('GET -> URL_BASE, should return res.status code 200, res.body to be defined, ande res.body.length === 1, res.body[0].categoryId === category.id, res.body[0].category.id === category.id', async () => {
+    const res = await request(app)
+        .get(`${URL_BASE}?category=${category.id}`)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(1)
+
+    expect(res.body[0].categoryId).toBeDefined()
+    expect(res.body[0].categoryId).toBe(category.id)
+
+    expect(res.body[0].category).toBeDefined()
+    expect(res.body[0].category.id).toBe(category.id)
+})
+
 //GETONE
-test('GET -> URL_BASE/:id, should return res.status code 200, res.body to be defined and res.body.title === product.title', async () =>{
+test('GET -> URL_BASE/:id, should return res.status code 200, res.body to be defined and res.body.title === product.title, res.body.category.id to be defined, res.body.category.id === category.id', async () =>{
     const res = await request(app)
         .get(`${URL_BASE}/${productId}`)
     
     expect(res.status).toBe(200)
     expect(res.body).toBeDefined()
     expect(res.body.title).toBe(product.title)
+
+    expect(res.body.category.id).toBeDefined() 
+    expect(res.body.category.id).toBe(category.id)
 })
 //PUT
 test('PUT -> URL_BASE/:id, should return res.tatus code 200, res.bodyto be defined and res.body.title === "newtitle" ', async()=> {
