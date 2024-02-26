ec2a40baf8b768c6b55b035309e98d1f0592f7dd
Gmsantos2 <gmsantos2@utpl.edu.ec>
2024-02-26 13:05:51 -0500

    controller cart

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -39,9 +39,11 @@ const remove = catchError(async(req, res) => {
 
 const update = catchError(async(req, res) => {
     const { id } = req.params;
+    const {quantity} = req.body
+    const userId = req.user.id
     const result = await Cart.update(
-        req.body,
-        { where: {id}, returning: true }
+        {quantity},
+        { where: {id, userId}, returning: true }
     );
     if(result[0] === 0) return res.sendStatus(404);
     return res.json(result[1][0]);
